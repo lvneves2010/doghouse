712eecbfb45b2deb04dae5a8470e2ddc27c605c1
unknown <lvneves2010@gmail.com>
2019-05-08 06:51:30 -0300

    dog view

--- dogapp/views.py
+++ dogapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Dog, Customer
 
-# Create your views here.
+def list_dogs(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dogs.html', {'dogs': dogs})
+
+def create_dog(request):
+    form = DogForm(request.POST or None)
+
+    if form.is_valid()
+        form.save()
+        return redirect('list_dogs')
+
+    return render(request, 'products-form.html', {'form': form})
+
+def update_dog(request, id):
+    dog = Dog.objects.get(id=id)
+    form = DogForm(request.POST or None, instance=dog)
+
+    if form.is_valid()
+        form.save()
+        return redirect('list_dogs')
+
+    return render(request, 'products-form.html', {'form': form})
+
+
